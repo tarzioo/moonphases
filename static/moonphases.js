63d6ea002475c142c1294c847981915b94227291
Taraneh Tayaran <ttayaran@gmail.com>
2018-01-18 15:39:43 +0000

    modified shadow to not show glow in phases not like the 'new' phase

--- static/moonphases.js
+++ static/moonphases.js
@@ -1,6 +1,6 @@
-ar dict = {
+var dict = {
     new: "0px",
-    full: "610px",
+    full: "0px",
     waxingCrescent: "150px",
     waningCrescent: "-150px",
     firstQuarter: "300px",
@@ -10,24 +10,29 @@ ar dict = {
 };
 
 function setPhase(phase) {
-    
+        
     if(phase === "firstQuarter"){
-	$(".shadow").css({"right": dict[phase], "border-radius": "50% 0 0  50%", "height": "320px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none"});
-	}
+        $(".shadow").css({"right": dict[phase], "border-radius": "50% 0 0  50%", "height": "320px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none", "width": "300px"});
+    }
     else if(phase === "thirdQuarter"){
-	$(".shadow").css({"right": dict[phase], "border-radius": "0 50% 50% 0", "height": "320px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none"});
-	}
+        $(".shadow").css({"right": dict[phase], "border-radius": "0 50% 50% 0", "height": "320px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none", "width": "300px"});
+    }
     else if(phase === "waxingGibbous"){
-	$(".shadow").css({"right": dict[phase], "box-shadow": "inset 60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "rgba(0, 0, 0, 0)" });
-	}
+        $(".shadow").css({"right": dict[phase], "box-shadow": "inset 60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "rgba(0, 0, 0, 0)" , "width": "320px"});
+    }
     
     else if(phase === "waningGibbous"){
-	$(".shadow").css({"right": dict[phase], "box-shadow": "inset -60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "rgba(0, 0, 0, 0)"});
-	
-	}
+        $(".shadow").css({"right": dict[phase], "box-shadow": "inset -60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "rgba(0, 0, 0, 0)", "width": "320px"});
+    
+    }
+    
+        else if(phase === "full"){
+        $(".shadow").css({"right": dict[phase], "box-shadow": "none", "background-color": "rgba(0, 0, 0, 0)", "border-radius": "50%", "width": "300px"});
+    
+    }
     else {
-	$(".shadow").css({"right": dict[phase], "border-radius": "50%", "height": "300px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none" });
-	}
+        $(".shadow").css({"right": dict[phase], "border-radius": "50%", "height": "300px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none" , "width": "300px"});
+    }
     
     
 };
