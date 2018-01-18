e3956ebda54c892ea796b6c6196b00931b08c0b5
Taraneh Tayaran <ttayaran@gmail.com>
2018-01-18 01:32:44 +0000

    updated files to now include moonphases for waxing Gibbous and Waning Gibbous

--- static/moonphases.js
+++ static/moonphases.js
@@ -1,36 +1,32 @@
 var dict = {
-	new: "0px",
-	full: "610px",
-	waxingCrescent: "150px",
-	waningCrescent: "-150px",
-	firstQuarter: "300px",
-	thirdQuarter: "-300px",
+    new: "0px",
+    full: "610px",
+    waxingCrescent: "150px",
+    waningCrescent: "-150px",
+    firstQuarter: "300px",
+    thirdQuarter: "-300px",
+    waxingGibbous: "0px",
+    waningGibbous: "0px",
 };
 function setPhase(phase) {
-		
-	if(phase === "firstQuarter"){
-		$(".shadow").css({"right": dict[phase], "border-radius": "50% 0 0  50%", "height": "320px"});
+    
+    if(phase === "firstQuarter"){
+	$(".shadow").css({"right": dict[phase], "border-radius": "50% 0 0  50%", "height": "320px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none"});
 	}
-	else if(phase === "thirdQuarter"){
-		$(".shadow").css({"right": dict[phase], "border-radius": "0 50% 50% 0", "height": "320px"});
+    else if(phase === "thirdQuarter"){
+	$(".shadow").css({"right": dict[phase], "border-radius": "0 50% 50% 0", "height": "320px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none"});
 	}
-	else {
-		$(".shadow").css({"right": dict[phase], "border-radius": "50%", "height": "300px"});
+    else if(phase === "waxingGibbous"){
+	$(".shadow").css({"right": dict[phase], "box-shadow": "inset 60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "none" });
 	}
+    
+    else if(phase === "waningGibbous"){
+	$(".shadow").css({"right": dict[phase], "box-shadow": "inset -60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "none" });
 	
+	}
+    else {
+	$(".shadow").css({"right": dict[phase], "border-radius": "50%", "height": "300px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none" });
+	}
+    
 };
 
-// function firstQuarter(){
-// 	$(".shadow").css({"right": "295px", "border-radius": "50% 0% 0% 50%", "height": "320px"});
-// }
-
-
-
-
-
-
-// $(".shadow").css({"border-radius": "50%", "height": "300px"});
-// $(".shadow").css("right", "150px");
-
-
-$(".shadow").css("right", "0px");
