640768a7bcaa4ec338384f0d99280f246c9cc283
Taraneh Tayaran <ttayaran@gmail.com>
2018-01-18 02:23:09 +0000

    added a phase timer to show different moon phases

--- static/moonphases.js
+++ static/moonphases.js
@@ -1,4 +1,4 @@
-var dict = {
+ar dict = {
     new: "0px",
     full: "610px",
     waxingCrescent: "150px",
@@ -8,6 +8,7 @@ var dict = {
     waxingGibbous: "0px",
     waningGibbous: "0px",
 };
+
 function setPhase(phase) {
     
     if(phase === "firstQuarter"){
@@ -17,16 +18,30 @@ function setPhase(phase) {
 	$(".shadow").css({"right": dict[phase], "border-radius": "0 50% 50% 0", "height": "320px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none"});
 	}
     else if(phase === "waxingGibbous"){
-	$(".shadow").css({"right": dict[phase], "box-shadow": "inset 60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "none" });
+	$(".shadow").css({"right": dict[phase], "box-shadow": "inset 60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "rgba(0, 0, 0, 0)" });
 	}
     
     else if(phase === "waningGibbous"){
-	$(".shadow").css({"right": dict[phase], "box-shadow": "inset -60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "none" });
+	$(".shadow").css({"right": dict[phase], "box-shadow": "inset -60px -10px 10px 10px rgba(0, 0, 0, 0.90)", "background-color": "rgba(0, 0, 0, 0)"});
 	
 	}
     else {
 	$(".shadow").css({"right": dict[phase], "border-radius": "50%", "height": "300px", "background-color": "rgba(0, 0, 0, 0.90)", "box-shadow": "none" });
 	}
     
+    
 };
+
+
+var phases = ["firstQuarter", "waxingCrescent", "new", "waningCrescent", "thirdQuarter", "waningGibbous", "full", "waxingGibbous"];
+ 
+
+for(var start = 0; start < phases.length; start++) {
+  (function(i){
+    setTimeout(function(){
+     setPhase(phases[i])
+    }, 1500 * i)
+  })(start);
+}
+
 
